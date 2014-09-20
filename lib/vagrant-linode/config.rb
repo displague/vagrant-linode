e733fddc7f52fb638601d02726ddbedc7e8ec6dc
Skoobiekins <scub@darchoods.net>
2014-09-19 20:02:02 -0400

    Exposing Disk Image allocation hooks through config

--- lib/vagrant-linode/config.rb
+++ lib/vagrant-linode/config.rb
@@ -11,6 +11,8 @@ module VagrantPlugins
       attr_accessor :ca_path
       attr_accessor :ssh_key_name
       attr_accessor :setup
+      attr_accessor :xvda_size
+      attr_accessor :swap_size
 
       alias_method :setup?, :setup
 
@@ -25,6 +27,8 @@ module VagrantPlugins
         @ca_path            = UNSET_VALUE
         @ssh_key_name       = UNSET_VALUE
         @setup              = UNSET_VALUE
+        @xvda_size          = UNSET_VALUE
+        @swap_size          = UNSET_VALUE
       end
 
       def finalize!
@@ -38,6 +42,8 @@ module VagrantPlugins
         @ca_path            = nil if @ca_path == UNSET_VALUE
         @ssh_key_name       = 'Vagrant' if @ssh_key_name == UNSET_VALUE
         @setup              = true if @setup == UNSET_VALUE
+        @xvda_size          = true if @xvda_size == UNSET_VALUE
+        @swap_size          = '256' if @swap_size == UNSET_VALUE 
       end
 
       def validate(machine)
