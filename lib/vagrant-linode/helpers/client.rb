0abbd5caad5e352390c51ffcc4b534b3893f496c
Marques Johansson <marques@displague.com>
2014-10-07 23:50:19 -0400

    remove unused delete/request/post methods from helpers::client

--- lib/vagrant-linode/helpers/client.rb
+++ lib/vagrant-linode/helpers/client.rb
@@ -40,64 +40,6 @@ module VagrantPlugins
 
         attr_reader :client
 
-        def delete(path, params = {}, _method = :delete)
-          @client.request :url_encoded
-          request(path, params, :delete)
-        end
-
-        def post(path, params = {}, _method = :post)
-          @client.headers['Content-Type'] = 'application/json'
-          request(path, params, :post)
-        end
-
-        def request(path, params = {}, method = :get)
-          begin
-            @logger.info "Request: #{path}"
-            result = @client.send(method) do |req|
-              req.url path, params
-              req.headers['Authorization'] = "Bearer #{@config.api_key}"
-            end
-          rescue Faraday::Error::ConnectionFailed => e
-            # TODO this is suspect but because farady wraps the exception
-            #      in something generic there doesn't appear to be another
-            #      way to distinguish different connection errors :(
-            if e.message =~ /certificate verify failed/
-              raise Errors::CertificateError
-            end
-
-            raise e
-          end
-
-          unless method == :delete
-            begin
-              body = JSON.parse(result.body)
-              @logger.info "Response: #{body}"
-              next_page = body['links']['pages']['next'] rescue nil
-              unless next_page.nil?
-                uri = URI.parse(next_page)
-                new_path = path.split('?')[0]
-                next_result = request("#{new_path}?#{uri.query}")
-                req_target = new_path.split('/')[-1]
-                body["#{req_target}"].concat(next_result["#{req_target}"])
-              end
-            rescue JSON::ParserError => e
-              raise(Errors::JSONError, message: e.message,
-                                       path: path,
-                                       params: params,
-                                       response: result.body)
-            end
-          end
-
-          unless /^2\d\d$/ =~ result.status.to_s
-            fail(Errors::APIStatusError, path: path,
-                                         params: params,
-                                         status: result.status,
-                                         response: body.inspect)
-          end
-
-          Result.new(body)
-        end
-
         def wait_for_event(env, id)
           retryable(tries: 120, sleep: 10) do
             # stop waiting if interrupted
