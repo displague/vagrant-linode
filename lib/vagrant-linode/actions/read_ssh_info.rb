c577edd7c3231ea0f35154bb3a48a41bba150d0a
Marques Johansson <marques@displague.com>
2014-12-03 00:18:14 -0500

    read_ssh_info tweaks to make vagrant up complete (too permissive on state checking?)

--- lib/vagrant-linode/actions/read_ssh_info.rb
+++ lib/vagrant-linode/actions/read_ssh_info.rb
@@ -7,12 +7,14 @@ module VagrantPlugins
       # `:machine_ssh_info` key in the environment.
       class ReadSSHInfo
         def initialize(app, _env)
+	  @env = _env
           @app    = app
+	  @machine = _env[:machine]
           @logger = Log4r::Logger.new('vagrant_linode::action::read_ssh_info')
         end
 
         def call(env)
-          env[:machine_ssh_info] = read_ssh_info(env[:linode_api], env[:machine])
+          env[:machine_ssh_info] = read_ssh_info(env[:linode_api], @machine)
 
           @app.call(env)
         end
@@ -20,8 +22,9 @@ module VagrantPlugins
         def read_ssh_info(_linode, machine)
           return nil if machine.id.nil?
           server = Provider.linode(machine, refresh: true)
+	  @env[:ui].info "Machine State ID: %s" % [machine.state.id]
 
-          return nil if machine.state.id != :active # @todo this seems redundant to the next line.
+          #return nil if machine.state.id != :active # @todo this seems redundant to the next line. may be more correct.
           if server.nil?
             # The machine can't be found
             @logger.info("Machine couldn't be found, assuming it got destroyed.")
@@ -30,6 +33,7 @@ module VagrantPlugins
           end
 
           public_network = server.network.find { |network| network['ispublic'] == 1 }
+	  @env[:ui].info "IP Address: %s" % public_network['ipaddress']
 
           {
             host: public_network['ipaddress'],
