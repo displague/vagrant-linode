68a288cc9312a7e74f663de29fc40f3babbebc36
Marques Johansson <marques@displague.com>
2014-12-25 16:08:48 -0500

    add size and created dt to image list

--- lib/vagrant-linode/actions/list_images.rb
+++ lib/vagrant-linode/actions/list_images.rb
@@ -8,9 +8,9 @@ module VagrantPlugins
 
         def call(env)
           linode_api = env[:linode_api]
-          env[:ui].info ('%-36s %s' % ['Image ID', 'Image Label'])
+          env[:ui].info ('%-10s %-22s %-10s %s' % ['Image ID', 'Created', 'Size (MB)', 'Image Label'])
           linode_api.image.list.sort_by(&:imageid).each do |img|
-            env[:ui].info ('%-36s %s' % [img.imageid, img.label])
+            env[:ui].info ('%-10s %-22s %-10s %s' % [img.imageid, img.create_dt, img.minsize, img.label])
           end
           @app.call(env)
         end
